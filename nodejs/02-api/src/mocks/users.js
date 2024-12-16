5e668c344d934a4c60fc5dc704e5df2c1183feb3
natanzeraa <natanzeraaaaaa@gmail.com>
2024-12-16 15:41:33 -0300

    feat(02-api): add users.js mock data

---
+++ nodejs/02-api/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Natan Oliveira'
+    },
+    {
+        id: 2,
+        name: 'Elizangela Mendes'
+    },
+    {
+        id: 3,
+        name: 'Megh Bravo'
+    }
+]
