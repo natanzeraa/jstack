bf45183fd3b5868b415204e23f4e4283b3f469e9
natanzeraa <natanzeraaaaaa@gmail.com>
2024-12-16 23:14:04 -0300

    feat: add show and delete routes

--- nodejs/03-express-js/src/routes.js
+++ nodejs/03-express-js/src/routes.js
@@ -4,6 +4,8 @@ import ContactController from './app/controllers/ContactController.js'
 const router = Router()
 
 router.get('/contacts', ContactController.index)
+router.get('/contacts/:id', ContactController.show)
+router.delete('/contacts/:id', ContactController.delete)
 
 
 export default router
