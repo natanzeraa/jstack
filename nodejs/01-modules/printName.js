cd6fbf7067a6d47c525ee5b7850d8582421160a2
natanzeraa <natanzeraaaaaa@gmail.com>
2024-12-18 09:47:06 -0300

    delete: moved folders one level up

--- nodejs/01-modules/printName.js
+++
@@ -1,5 +0,0 @@
-exports.printName = (name) => {
-    console.log(name)
-}
-
-exports.lastName = 'Oliveira'
--- nodejs/03-express-js/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import router from './routes.js'
-
-const app = express()
-
-app.use(express.json())
-app.use(router)
-
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
